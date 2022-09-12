af0008c9b156a5376a38f2874d13dc11624e4520
yousuf-cse-bd <yousuf.cse17@gmail.com>
2022-09-12 15:51:48 +0600

    This bubble sorting technique using two different arrays

--- BubbleSortNext.cpp
+++ BubbleSortNext.cpp
@@ -1,25 +1,22 @@
 /**
  * @file BubbleSortNext.cpp
  * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
- * @brief The best bubble sorting technique best.
+ * @brief This bubble sorting technique using two different arrays
  * @version 0.1
  * @date 2022-07-13
  * @since Thursday, 9.30PM
  * @copyright Copyright (c) 2022
- * 
+ * Dept. of CSE, Varendra University, Rajshahi, Bangladesh
  */
 
 #include <iostream>
 using namespace std;
 /*Descending order bubble sort algorithm implementaion*/
-void bubbleSort(int A[], unsigned int n){
-    bool swapSatus = true;
-    for(unsigned int i = 0; i<n - 1 && swapSatus == true; i++){
-        swapSatus = false;
-        for(unsigned int j = i+1; j<n; j++){
-            if(A[i] > A[j]){
-                swap(A[i], A[j]);
-                swapSatus = true;
+void bubbleSortNext(int *Array, const unsigned int size){
+    for(unsigned int i = 0; i< size - 1; i++){
+        for(unsigned int j = i+1; j < size; j++){
+            if(*(Array+i) < *(Array+j)){
+                swap(*(Array+i), *(Array+j));
             }
         }
     }
@@ -27,12 +24,27 @@ void bubbleSort(int A[], unsigned int n){
 int main(int argc, char const *argv[])
 {
     /* code */
-    int A[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11};
-    unsigned int n = sizeof(A) / sizeof(int);
-    bubbleSort(A, n);
-    /*Print the result*/
-    for(auto x : A){
-        cout<<x<<" ";
+    system("cls");
+    #ifndef TestCaseMatching
+        freopen("InputData.txt", "r", stdin);
+        freopen("Output.txt", "w", stdout);
+    #endif
+    // int A[] = {2, 1, 5, 4, 3};
+    const unsigned int size = 5;
+    
+    int A[size];
+    int testCase = 1;
+    while(testCase <= 120){
+        cout<<testCase<<": Case = ";
+        for(unsigned int i = 0; i < size; i++){
+            cin>>*(A+i);
+        }
+        bubbleSortNext(A, size);
+        for(int x : A){
+            cout<<x<<" ";
+        }
+        cout<<endl;
+        testCase++;
     }
     return 0;
 }
