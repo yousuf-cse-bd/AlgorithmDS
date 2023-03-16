13222fb71e048b146a7710242cda4e3ee9fdc186
yousuf-cse-bd <yousuf.cse17@gmail.com>
2023-03-16 16:59:05 +0600

    A demo perform as online judge

---
+++ OnlineJudgeDemo.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file OnlineJudgeDemo.cpp
+ * @author Md. Yousuf Ali (yousuf.cse17@gmail.com)
+ * @brief A demo perform as online judge
+ * @version 0.1
+ * @date 2023-03-16
+ * @since ThursDay; 09:34 AM
+ * @copyright Copyright (c) 2023
+ * @institute: Dept. of CSE, Varendra University, Rajshahi, Bangladesh
+ */
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char *const argv[]){
+	/* code */
+	system("cls");
+	#ifndef MY_ONLINE_JUDGE
+	freopen("Input1.txt", "r", stdin);
+	freopen("Output1.txt", "w", stdout);
+	#endif
+	int num1, num2;
+	cout<<num1+num2<<endl;
+	return 0;
+}
